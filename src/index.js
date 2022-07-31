aa6d01be0279b8436d66a554faf2c57173d2367d
alina-kam <alinakamenets08@gmail.com>
2022-07-31 16:20:32 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+    let nArr = Math.abs(n).toString().split('');
+    let temp = '';
+
+    for (i=0; i < Math.ceil(nArr.length) / 2; i++){
+        temp = nArr[i];
+        nArr[i] = nArr[nArr.length-1-i]
+        nArr[nArr.length-1-i] = temp;
+    };
+
+    return +nArr.join('');
 }
+
+console.log(module.exports(71348));
